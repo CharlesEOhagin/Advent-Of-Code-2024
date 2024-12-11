fc60f94c1f084bef0b2e5467ca9bbf4efb4ef474
CharlesEOhagin <charles.ohagin@gmail.com>
2024-12-11 11:20:46 -0800

    finished day 1 part 2 challenge

--- day1.py
+++ day1.py
@@ -16,3 +16,13 @@ sum = 0
 for i in range(len(list1)):
     sum += abs(list1[i] - list2[i])
 print(sum)
+sum2 = 0
+explored = []
+for i in range(len(list1)):
+    list1[i]
+    for j in range(len(list2)):
+        count = 0
+        if list2[j] == list1[i]:
+            count += 1
+        sum2 += count * list1[i]
+print(sum2)
